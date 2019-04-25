6f39175070392dc78f17b131bb56b1f58e292065
Your Name <you@example.com>
2019-04-25 17:09:40 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from app import app, movie_recommand
-from flask import render_template
-from flask import jsonify, url_for
+from app import app
+from flask import jsonify
+#!/usr/bin/python
 
 
 @app.route('/')
@@ -12,35 +12,11 @@ def index():
     return jsonify(response)
 
 
-# 只获取整型ID
-@app.route('/player/<int:player_id>')
-def show_post(player_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % (player_id+1)
-
-# url_for生成匹配url
-
-
-@app.route('/articles')
-def api_articles():
-    return 'List of ' + url_for('api_articles')
-
-
-@app.route('/getMsg', methods=['POST'])
+@app.route('/getMsg', methods=['GET'])
 def home():
+
     response = {
         'name': 'Nirvazure',
         'hero': ['ganjiang', 'gongsunli', 'mozi']
     }
     return jsonify(response)
-
-
-@app.route('/search')
-def search():
-    user = {'nickname': 'Miguel'}  # fake user
-    movie_name = 'Twelve Monkeys (1995)'
-    posts = movie_recommand.web_api(movie_name, 15)
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
