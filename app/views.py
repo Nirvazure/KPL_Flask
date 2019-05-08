02b13e9bc211d39c0a2718460f052515818965e3
Your Name <you@example.com>
2019-05-08 11:01:03 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,21 +1,32 @@
-from app import app, spider
-from flask import jsonify
+from app import spider, app
+from flask import jsonify,render_template,url_for
 
 spider = spider.Spider()
 heros = spider.getHeros()
-
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return jsonify(heros)
 
+@app.route('/search')
+def search():
+    user = {'nickname': 'Miguel'}  # fake user
+    # movie_name = 'Twelve Monkeys (1995)'
+    # posts = movie_recommand.web_api(movie_name, 15)
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           #    posts=posts
+                           )
 
-@app.route('/getMsg', methods=['GET'])
-def home():
+# 只获取整型ID
+@app.route('/player/<int:player_id>')
+def show_post(player_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % (player_id+1)
 
-    response = {
-        'name': 'Nirvazure',
-        'hero': ['ganjiang', 'gongsunli', 'mozi']
-    }
-    return jsonify(response)
+# url_for生成匹配url
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
